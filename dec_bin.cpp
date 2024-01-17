1d99cce1aac7dcc2a803a67d0f5147e1dfa7177a
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-01-17 16:00:58 +0530

    Update dec_bin.cpp

--- dec_bin.cpp
+++ dec_bin.cpp
@@ -1,1 +1,22 @@
 
+///decimal to binary : 5->101, tc=o(log(n))=sc
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void dec_bin(int n){
+    if(n > 0){
+        dec_bin(n/2);
+        cout << n % 2;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "Binary form of decimal num " << n << " : ";
+    dec_bin(n);
+    cout << endl;
+
+    return 0;
+}
